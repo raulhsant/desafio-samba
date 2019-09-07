28488e43ddf008cf6631736a16fb671da3fb1833
Raul Santana <raulhsantana@gmail.com>
2019-09-07 11:23:05 -0300

    initial commit - project creation

---
+++ src/main/java/com/sambatech/challenge/controller/UploadController.java
@@ -0,0 +1,9 @@
+package com.sambatech.challenge.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UploadController {
+
+}
